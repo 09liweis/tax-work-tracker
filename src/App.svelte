035369e8dabba6ddbc1ec8a51d6948b01e878766
Sam Li <weisen.li@hotmail.com>
2026-02-19 15:11:57 -0500

    Start repository

---
+++ src/App.svelte
@@ -0,0 +1,12 @@
+<script lang="ts">
+  import Router from 'svelte-spa-router';
+  import Login from './routes/Login.svelte';
+  import Dashboard from './routes/Dashboard.svelte';
+
+  const routes = {
+    '/': Login,
+    '/dashboard': Dashboard,
+  };
+</script>
+
+<Router {routes} />
